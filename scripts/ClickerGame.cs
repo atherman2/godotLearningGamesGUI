11b4aae5f2a295ea74764ad5b769fdc78c3a07d4
atherman2 <alexandre.t.herman@gmail.com>
2025-02-04 14:32:05 -0300

    initialization of ClickeGame class

--- scripts/ClickerGame.cs
+++ scripts/ClickerGame.cs
@@ -4,24 +4,49 @@ using System.Collections.Generic;
 
 public partial class ClickerGame : Node
 {
-	int money = 0;
+	int money, collectorIndex;
 	public ClickerUI ui;
 	public List<Collector> collectors;
 
-	struct CollectorInfo{int outcome; double coolDown; string infoText;};
+	struct CollectorInfo
+	{
+		public int outcomeValue; public double coolDownTime; public string infoTextString;
+		public CollectorInfo(int outcome, double coolDown, string infoText)
+		{
+			outcomeValue = outcome; coolDownTime = coolDown; infoTextString = infoText;
+		}
+	};
 	private List<CollectorInfo> collectorsInfo;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		money = 0;
+		collectorIndex = -1;
+		DefineCollectorsInfo();
 		ui = GetNode<ClickerUI>("ClickerUI");
-		ui.AddCollectorUI("Basic\nCollector");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		UpdateCoolDown();
+	}
+
+	private void DefineCollectorsInfo()
+	{
+		collectorsInfo = new List<CollectorInfo>
+		{
+			new (1, 2.0, "Coletor\nBásico")
+		};
+	}
+
+	public void AddCollector()
+	{
+		collectorIndex++;
+		CollectorInfo collectorInfo = collectorsInfo[collectorIndex];
+		ui.AddCollectorUI(collectorInfo.infoTextString);
+		collectors.Add(new Collector(collectorInfo.outcomeValue, collectorInfo.coolDownTime));
 	}
 
 	public void Collect(int index)
